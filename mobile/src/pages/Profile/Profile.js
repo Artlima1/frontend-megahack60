abab4f1e31e17e7c01102a4d4fffa514fa0447fd
Arthur Martins Braga <arthurmbraga1@gmail.com>
2020-07-05 00:28:44 -0300

    feat: corrections on profile page

--- mobile/src/pages/Profile/Profile.js
+++ mobile/src/pages/Profile/Profile.js
@@ -1,69 +1,114 @@
-import React, { useContext } from 'react'
-import AuthContext from '../../context'
+import React, { useContext } from "react";
+import AuthContext from "../../context";
 
-import { AntDesign, Entypo, Ionicons, Foundation } from '@expo/vector-icons';
-import styles from './ProfileStyle'
+import { AntDesign, Entypo, Ionicons, Foundation } from "@expo/vector-icons";
+import styles from "./ProfileStyle";
 
-import { Text, View, Button, TouchableOpacity, Image } from 'react-native';
+import { Text, View, Button, TouchableOpacity, Image } from "react-native";
 
+export default function Profile({ navigation }) {
+  const { signOut } = useContext(AuthContext);
 
-export default function Profile({navigation}){
-    const { signOut } = useContext(AuthContext)
-
-const ProfileButtons = ({onPress, title}) => (
+  const ProfileButtons = ({ onPress, title, subtitle, icon }) => (
     <TouchableOpacity onPress={onPress} style={styles.optionsButtonContainer}>
-            <Text style={styles.optionsButtonText}>{title}</Text>
+      {icon}
+      <Text style={styles.optionsButtonText}>{title}</Text>
+      <Text style={styles.text}>{subtitle}</Text>
     </TouchableOpacity>
-);
+  );
 
+  return (
+    <View style={styles.container}>
+      <View style={styles.profileHeader}>
+        <Image
+          style={styles.logo}
+          source={require("../../images/friday_1.png")}
+        />
+      </View>
 
-    return(
-        <View style={styles.container}>
-                <View style={styles.profileHeader}> 
-                    <Text style={styles.profileTitle}>PERFIL</Text>
-                </View>
+      <View style={styles.userStyle}>
+        <Text style={styles.userText}>Tabata Silva Carneiro</Text>
+        <Image
+          style={styles.userImage}
+          source={require("../../images/userImage.png")}
+        />
+      </View>
 
-                <View style={styles.userStyle}>
-                    <Text style={styles.userText}>Tabata Silva Carneiro</Text>
-                    <Image style={styles.userImage} source={require('../../images/userImage.png')} />
-                </View>
+      <View style={styles.profileButtons}>
+        <ProfileButtons
+          title="Bares Curtidos"
+          subtitle="Ver meus bares curtidos"
+          onPress={() => {
+            navigation.push("FavBars");
+          }}
+          icon={
+            <AntDesign
+              name="heart"
+              size={24}
+              color="#FFFFFF"
+              style={styles.icon}
+            />
+          }
+        />
+        <View style={styles.whiteDivider} />
 
-            <View style={styles.profileButtons}>
-                <AntDesign name="heart" size={24} color="#FFFFFF" style={styles.heartIcon} />
-                <ProfileButtons  title='Bares Curtidos' onPress={()=>{navigation.push('FavBars')}}/> 
-                <Text style={styles.text}>Ver meus bares curtidos</Text>
-                <View style={styles.whiteDivider} />
+        <ProfileButtons
+          title="Métodos de Pagamento"
+          subtitle="Meus métodos de pagamentos"
+          onPress={() => {
+            navigation.push("PaymentMethods");
+          }}
+          icon={
+            <Entypo
+              name="credit-card"
+              size={24}
+              color="#FFFFFF"
+              style={styles.icon}
+            />
+          }
+        />
+        <View style={styles.whiteDivider} />
+        <ProfileButtons
+          title="Ajuda"
+          subtitle="Aperte em caso de assédio e notifique o bar"
+          onPress={() => {
+            navigation.push("Help");
+          }}
+          icon={
+            <Entypo
+              name="help-with-circle"
+              size={24}
+              color="#FFFFFF"
+              style={styles.icon}
+            />
+          }
+        />
 
-                <Entypo name="credit-card" size={24} color="#FFFFFF" style={styles.cardIcon} />
-                <ProfileButtons title='Métodos de Pagamento' onPress={()=>{navigation.push('PaymentMethods')}}/>
-                <Text style={styles.text}>Meus métodos de pagamentos</Text>
-                <View style={styles.whiteDivider} />
+        <View style={styles.whiteDivider} />
 
-                <Foundation name="alert" size={24} color="#FFFFFF" style={styles.alertIcon} />
-                <ProfileButtons title='FAQ' onPress={()=>{navigation.push('FAQ')}}/>
-                <Text style={styles.text}>Perguntas mais frequentes</Text>
-                <View style={styles.whiteDivider} />
+        <ProfileButtons
+          title="Sair"
+          subtitle="Desconectar da sua conta"
+          onPress={() => {
+            navigation.push("Help");
+          }}
+          icon={
+            <Ionicons
+              name="md-exit"
+              size={24}
+              color="#FFFFFF"
+              style={styles.icon}
+            />
+          }
+        />
 
-                <Entypo name="help-with-circle" size={24} color="#FFFFFF" style={styles.helpIcon} />
-                <ProfileButtons title='Ajuda' onPress={()=>{navigation.push('Help')}}/>
-                <Text style={styles.text}>Aperte em caso de assédio e notifique o bar</Text>
-                <View style={styles.whiteDivider} />
+        <View style={styles.whiteDivider} />
 
-                <Ionicons name="md-exit" size={24} color="#FFFFFF" style={styles.exitIcon} />
-                <ProfileButtons title='Sair' onPress={()=>{signOut()}}/>
-                <Text style={styles.text}>Ver meus bares curtidos</Text>
-                <View style={styles.whiteDivider} />
-
-                <Image style={styles.logoImage} source={require('../../images/logo.png')} />
-            </View>
-        </View>
-    )
+        <Image
+          style={styles.logoImage}
+          source={require("../../images/logo.png")}
+        />
+      </View>
+    </View>
+  );
 }
-
-
-
-
-
-
-
-
--- mobile/src/pages/Profile/ProfileStyle.js
+++ mobile/src/pages/Profile/ProfileStyle.js
@@ -1,91 +1,98 @@
-import { StyleSheet } from 'react-native'
-
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#000000',
-    },
-    profileTitle: {
-        fontSize: 45,
-        color: '#F4AA1D',
-        // shadowColor: "#000",
-        // shadowRadius: 3.84,
-        // elevation: 8,
-    },
-    profileHeader: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#455056',
-        height: 160,
-    },
-    text: {
-        fontSize: 12,
-        color: '#FFFFFF',
-        marginLeft: 50,
-    },
-    heartIcon: {
-        position: 'absolute',
-        marginTop: 30,
-        marginLeft: 15,
-    },
-    cardIcon: {
-        position: 'absolute',
-        marginTop: 115,
-        marginLeft: 15,
-    },
-    alertIcon: {
-        position: 'absolute',
-        marginTop: 200,
-        marginLeft: 15,
-    },
-    helpIcon: {
-        position: 'absolute',
-        marginTop: 280,
-        marginLeft: 15,
-    },
-    exitIcon: {
-        position: 'absolute',
-        marginTop: 365,
-        marginLeft: 15,
-    },
-    optionsButtonText: {
-        color: '#FFFFFF',
-        fontSize: 16,
-        fontWeight: 'bold',
-        marginTop: 20,
-        marginLeft: 50,
-    },
-    whiteDivider: {
-        backgroundColor: '#FFFFFF',
-        height: 1,
-        width: 300,
-        alignSelf: 'center',
-        marginTop: 20,
-    },
-    userStyle: {
-        flexDirection: 'row-reverse',
-        backgroundColor: '#FB0C06',
-        height: 33,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: 20,
-    },
-    userText: {
-        color: '#FFFFFF',
-        fontSize: 25,
-        fontWeight: 'bold'
-    },
-    logoContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    logoImage: {
-        flex: 1,
-        width: 200,
-        height: 200,
-        resizeMode: 'contain',
-        marginTop: 170,
-        alignSelf: 'center',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+  },
+  profileTitle: {
+    fontSize: 45,
+    color: "#F4AA1D",
+    // shadowColor: "#000",
+    // shadowRadius: 3.84,
+    // elevation: 8,
+  },
+  optionsButtonContainer: {
+    position: "relative",
+  },
+  profileHeader: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
+    height: 160,
+  },
+  logo: {
+    resizeMode: "contain",
+    height: "60%",
+  },
+  text: {
+    fontSize: 12,
+    color: "#FFFFFF",
+    marginLeft: 50,
+    textAlignVertical: "top",
+  },
+  icon: {
+    position: "absolute",
+    marginTop: 30,
+    marginLeft: 15,
+  },
+  cardIcon: {
+    position: "absolute",
+    marginTop: 115,
+    marginLeft: 15,
+  },
+  alertIcon: {
+    position: "absolute",
+    marginTop: 200,
+    marginLeft: 15,
+  },
+  helpIcon: {
+    position: "absolute",
+    marginTop: 280,
+    marginLeft: 15,
+  },
+  exitIcon: {
+    position: "absolute",
+    marginTop: 365,
+    marginLeft: 15,
+  },
+  optionsButtonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginLeft: 50,
+  },
+  whiteDivider: {
+    backgroundColor: "#FFFFFF",
+    height: 1,
+    width: 300,
+    alignSelf: "center",
+    marginTop: 20,
+  },
+  userStyle: {
+    flexDirection: "row-reverse",
+    backgroundColor: "#FB0C06",
+    height: 33,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  userText: {
+    color: "#FFFFFF",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  logoContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logoImage: {
+    flex: 1,
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginTop: 170,
+    alignSelf: "center",
+  },
 });
