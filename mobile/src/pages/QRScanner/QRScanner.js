40d6c2412926d4c0aa71d7a218a42cd355c50f66
Arthur Martins Braga <arthurmbraga1@gmail.com>
2020-07-05 13:00:24 -0300

    feat: qr code working

--- mobile/src/pages/QRScanner/QRScanner.js
+++ mobile/src/pages/QRScanner/QRScanner.js
@@ -1,24 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { IconButton, Button } from "react-native-paper";
+import Modal from "react-native-modal";
+import TextInput from "../../Components/TextInput";
+import api from "../../Services/api";
 // import styles from './QRScannerStyle';
 
 export default function QRScanner() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [modal, setModal] = useState(true);
+  const [code, setCode] = useState();
 
   useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    validate(data);
   };
+
+  async function validate(data) {
+    try {
+      const response = await api.post("order_sheets", {
+        payment_method: "Cartao",
+        tables_id: data,
+      });
+      alert("Mesa encontrada com sucesso!");
+
+      const response2 = await api.get(`order_sheets_id/${response.data}`);
+      console.log(response2.data);
+
+    } catch (error) {
+      console.log(error.response);
+      alert("Mesa não encontrada");
+    }
+  }
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -31,15 +53,53 @@ export default function QRScanner() {
     <View
       style={{
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-      }}>
+        flexDirection: "column",
+        justifyContent: "flex-end",
+      }}
+    >
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
 
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      {scanned && (
+        <Button mode={"contained"} onPress={() => setScanned(false)}>
+          Escanear novamente
+        </Button>
+      )}
+      <IconButton
+        icon="alphabetical"
+        style={{ position: "absolute", top: 16, right: 16 }}
+        color="black"
+        size={35}
+        onPress={() => {
+          setModal(true);
+        }}
+      />
+      <Modal
+        isVisible={modal}
+        onBackButtonPress={() => setModal(false)}
+        onBackdropPress={() => setModal(false)}
+      >
+        <View style={{ backgroundColor: "#212121", padding: 16 }}>
+          <TextInput
+            label="Código da mesa"
+            onChangeText={(text) => {
+              setCode(text);
+            }}
+          />
+          <Button
+            mode={"contained"}
+            style={{ marginTop: 16 }}
+            onPress={() => {
+              setModal(false);
+              validate(code);
+            }}
+          >
+            Enviar
+          </Button>
+        </View>
+      </Modal>
     </View>
   );
 }
