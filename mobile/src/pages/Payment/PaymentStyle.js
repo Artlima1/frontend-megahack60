748c53c0ece72de60fab26e9df7773d30b768986
Carolina <carolinaalvares@cpejr.com.br>
2020-07-04 21:57:08 -0300

    feat: add all elements, page Payment

--- mobile/src/pages/Payment/PaymentStyle.js
+++ mobile/src/pages/Payment/PaymentStyle.js
@@ -8,35 +8,31 @@ export default StyleSheet.create({
     },
     paymentHeader: {
         flexDirection: 'row',
-        alignItems: 'center',
         justifyContent: 'space-between',
+        alignItems: 'center',
+
         padding: 20,
         backgroundColor: '#455056',
         height: 160,
     },
-    seusPedidosText1: {
-        color: '#FFFFFF',
-        fontSize: 15,
-        fontWeight: 'bold',
-        marginTop: 7,
-        marginLeft: 10,
-    },
-    seusPedidosText2: {
-        color: '#FFFFFF',
-        fontSize: 15,
-        fontWeight: 'bold',
-        marginLeft: 10,
-    },
     seusPedidosContainer: {
-        flexDirection: 'row',
-        marginTop: 10
+        flexDirection: 'column',
+    },
+    seusPedidosText: {
+        color: '#FFFFFF',
+        fontSize: 11,
+        fontWeight: 'bold',
+        marginLeft: 10,
+    },
+    seusPedidosIcon: {
+        marginLeft: 20
     },
     paymentTitle: {
         fontSize: 40,
+        marginLeft: 15,
         color: '#F4AA1D',
     },
     finalizarPagamentoContainer: {
-        marginTop: 500,
         backgroundColor: '#FB0C06',
         height: 50,
         width: 250,
@@ -45,8 +41,88 @@ export default StyleSheet.create({
     },
     finalizarPagamentoText: {
         color: '#FFFFFF',
-        fontSize: 15,
+        fontSize: 25,
         fontWeight: 'bold',
         alignSelf: 'center',
+        marginTop: 5
     },
+    grayContainer: {
+        borderRadius: 12,
+        alignSelf: 'center',
+        justifyContent: 'space-around',
+        marginTop: 40,
+        width: 370,
+        height: 480,
+    },
+    grayColor: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 12,
+        alignSelf: 'center',
+        width: 370,
+        height: 480,
+        opacity: 0.3,
+        position: 'absolute',
+    },
+    totalText: {
+        color: '#FFFFFF',
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginLeft: 10,
+    },
+    totalText: {
+        alignSelf: 'center',
+        color: '#FFFFFF',
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
+    totalNumber: {
+        alignSelf: 'center',
+        color: '#FFFFFF',
+        fontSize: 50,
+        fontWeight: 'bold',
+    },
+    cardLine: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cardLineContainer: {
+        backgroundColor: '#FFFFFF',
+        color: '#FFFFFF',
+        width: 150,
+        alignItems: 'center',
+        borderRadius: 4,
+        opacity: 0.5,
+        marginLeft: 10
+    },
+    changeMethod: {
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+        textDecorationLine: 'underline',
+    },
+    cardNumber: {
+        color: '#FFFFFF',
+        fontSize: 10,
+    },
+    tip: {
+        alignSelf: 'center',
+        color: '#FFFFFF',
+        fontSize: 10,
+    },
+    tipContainer: {
+        backgroundColor: '#FFFFFF',
+        width: 150,
+        height: 60,
+        alignItems: 'center',
+        borderRadius: 4,
+        opacity: 0.5,
+        alignSelf: 'center',
+    },
+    tipText: {
+        alignSelf: 'center',
+        color: '#FFFFFF',
+        fontSize: 30,
+        fontWeight: 'bold',
+    }
 });
