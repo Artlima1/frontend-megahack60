dcb6cf55c7c036a6eb972e729bf9defdd083444c
Giovanna <giovanna.pimenta11@gmail.com>
2020-07-04 21:38:30 -0300

    feat: adding comments on checkout page

--- mobile/src/pages/CheckOut/CheckOut.js
+++ mobile/src/pages/CheckOut/CheckOut.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Text, View, Button, ImageBackground, StatusBar, Image } from "react-native";
+import { Text, TextInput, View, Button, ImageBackground, StatusBar, Image } from "react-native";
 import styles from "./CheckOutStyle.js";
 import Stars from 'react-native-stars';
 
 
 export default function CheckOut() {
+    const [text, setText] = useState('');
     return (
         <ImageBackground
             source={require("../../images/login.png")}
             style={styles.container}
         >
             <View style={styles.background}>
-                <Text style={{textAlign:'center', fontWeight:'bold'}}>Obrigado por comprar conosco,
+                <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Obrigado por comprar conosco,
                 espere o garçom chegar com sua nota fiscal
                 e a sessão será encerrada automaticamente!{"\n"}
-            </Text>
+                </Text>
                 <Text>Deixar avaliação para o bar do Manolo</Text>
                 <View style={styles.evaluation}>
                     <Text>Higiene e Limpeza:</Text>
@@ -97,6 +98,16 @@ export default function CheckOut() {
                         />
                     </View>
                 </View>
+
+                <View style={{ padding: 10, margin: 10 }}>
+                    <TextInput
+                        style={{ height: 40 }}
+                        placeholder="Comente aqui sobre a sua experiência!"
+                        onChangeText={text => setText(text)}
+                        defaultValue={text}
+                    />
+                </View>
+
             </View>
         </ImageBackground>
     );
